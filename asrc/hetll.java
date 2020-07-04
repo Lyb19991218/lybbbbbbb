ae611891e7ecc5397b03a2e33e541c9d23708ce0
ITliyibo <1175927960@qq.com>
2020-07-04 17:05:00 +0800

    第一次调剂

---
+++ asrc/hetll.java
@@ -0,0 +1,10 @@
+/**
+ * @PROJECT_NAME: Maven_parent
+ * @parse("File Header.java")
+ * @Author: LiYiBo
+ * @Date 2020/7/4 17:03
+ * @Version 1.0
+ */
+public class hetll {
+    private int hello;
+}
